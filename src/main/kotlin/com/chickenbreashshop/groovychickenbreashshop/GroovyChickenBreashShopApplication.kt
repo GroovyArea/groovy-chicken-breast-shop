e6789ff5caf090929331123c5913db329ba0827c
groovyarea <095201a@naver.com>
2023-06-16 23:59:59 +0900

    feat : new project init

---
+++ src/main/kotlin/com/chickenbreashshop/groovychickenbreashshop/GroovyChickenBreashShopApplication.kt
@@ -0,0 +1,11 @@
+package com.chickenbreashshop.groovychickenbreashshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroovyChickenBreashShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<GroovyChickenBreashShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/chickenbreashshop/groovychickenbreashshop/GroovyChickenBreashShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chickenbreashshop.groovychickenbreashshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroovyChickenBreashShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
